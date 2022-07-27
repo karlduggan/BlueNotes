6bcbaa0ad7bc9f79fcbce9314896ad7a4cc9efff
Karl Duggan <karlduggan_55@hotmail.com>
2022-07-27 20:24:21 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # django-learn
  
+## Django Notes
+
+$ python -m django --version
+$ django-admin startproject mysite
+$ python manage.py runserver
+
+$ django-admin startapp myapp
+
---
+++ backend/fooApp/views.py
@@ -0,0 +1,15 @@
+from urllib import response
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+# Create your views here.
+
+def index(request):
+    response = json.dumps([{}])
+    return HttpResponse(response, content_type="text/json")
+
+def foo(request):
+    response = json.dumps([{'test': 909090}])
+    return HttpResponse(response, content_type="text/json")
