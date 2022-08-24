771a64ac7a1fee4e932832278368e0dae743f804
Karl Duggan <karlduggan_55@hotmail.com>
2022-08-24 23:16:40 +0100

    Update HomePage.vue

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,5 +1,11 @@
 <template>
     <body>
+        <div class="hero-banner">
+            <h1>BluNotes</h1>
+
+            <h2>A simple ticket management system with a minimalistic design, 
+                built for the sole purpose of helping you get things done</h2>
+        </div>
         
     </body>
 
@@ -16,6 +22,14 @@ body {
     background-color: $background-color;
     // Off set the header with calc 
     min-height: calc(100vh - 68px);
+}
+.hero-banner {
+    padding: 100px;
+    color: $white;
+    width: 520px;
+}
+.hero-banner h2 {
+    font-size: 18px;
 }
 
 </style>
